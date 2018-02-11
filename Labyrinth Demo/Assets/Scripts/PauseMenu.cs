ae13a4a1f37f1718707aeb066fad7315e559686e
Allen Nguyen <nguye225@purdue.edu>
2018-02-11 13:15:36 -0500

    PauseMenu Additions
    Pause menu basic functionality is working with resume and quit.  Will wait on michael for restart help

---
+++ Labyrinth Demo/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public bool isPaused;
+    public GameObject pauseMenuCanvas;
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+		if(isPaused)
+        {
+            pauseMenuCanvas.SetActive(true);
+            Time.timeScale = 0f;
+        } else
+        {
+            pauseMenuCanvas.SetActive(false);
+            Time.timeScale = 1f;
+        }
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+	}
+    public void Resume()
+    {
+        isPaused = false;
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
