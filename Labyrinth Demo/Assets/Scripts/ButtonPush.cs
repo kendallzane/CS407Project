42819f8305feaa6880f6c7a10d6f5fabaa580aed
Justin B <bonnerj@purdue.edu>
2018-02-15 21:23:20 -0500

    Create button and canal prefabs

---
+++ Labyrinth Demo/Assets/Scripts/ButtonPush.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPush : MonoBehaviour {
+
+	public GameObject[] canalsToSwitch;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void onPress(){
+		foreach(GameObject canal in canalsToSwitch){
+			canal.GetComponent<ButtonLogic>().switchIt();
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.tag == "Sword") {
+			onPress (); 
+		} 
+	}
+			
+
+}
