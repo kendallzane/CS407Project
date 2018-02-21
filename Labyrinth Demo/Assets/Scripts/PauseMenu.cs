90955e0ad9f13acd803c12501b205b4ae1cd7883
Allen Nguyen <nguye225@purdue.edu>
2018-02-21 00:12:07 -0500

    Added Restart Button
    Added Restart Button to Pause Menu, if you want different types of styling, that can be something to work on for next sprint

--- Labyrinth Demo/Assets/Scripts/PauseMenu.cs
+++ Labyrinth Demo/Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour {
 
@@ -31,5 +32,9 @@ public class PauseMenu : MonoBehaviour {
     public void Quit()
     {
         Application.Quit();
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
