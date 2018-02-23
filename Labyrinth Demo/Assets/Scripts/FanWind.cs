4340a7c83ae4a29902aecc1b0f6ec5fe8fa4ce6b
Michael Parr <trumpetdude213@yahoo.com>
2018-02-22 21:26:00 -0500

    Fixed the Wind and Falling bugs
    Ben and I talked it through, changed up the fan code slightly to invert it, and fixed the falling animation permanent invinciblity bug

--- Labyrinth Demo/Assets/Scripts/FanWind.cs
+++ Labyrinth Demo/Assets/Scripts/FanWind.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FanWind : MonoBehaviour {
 
 	public float windForce;
+	public bool facingDown = true;
 	private bool containsPlayer;
 
 	private GameObject player;
@@ -19,11 +20,24 @@ public class FanWind : MonoBehaviour {
 	void Update () {
 		if (containsPlayer) {
 			Vector3 deltaPos = (player.transform.position - this.transform.position);
+
+			//Inversion
+			deltaPos = new Vector3 (1f / deltaPos.x, 1f / deltaPos.y, deltaPos.z);
+
 			deltaPos.Scale (new Vector3 (windForce, windForce, 1));
+
+			//Only apply in direction of wind travel
+			if (facingDown) {
+				//vertical wind	
+				deltaPos.x = 0;
+			} else {
+				//horizontal wind
+				deltaPos.y = 0;
+			}
 			deltaPos.z = 0;
 			player.transform.position = player.transform.position + deltaPos;
 		}
-		//containsPlayer = false;
+		containsPlayer = false;
 	}
 
 	void OnTriggerStay2D(Collider2D coll) {
