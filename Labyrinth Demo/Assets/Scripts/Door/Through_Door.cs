68a6695ab2650689d1c26a5b2a606d491253137f
brott97 <brott@purdue.edu>
2018-02-26 15:36:26 -0500

    removed unneeded code in Door.cs

--- Labyrinth Demo/Assets/Scripts/Door/Through_Door.cs
+++ Labyrinth Demo/Assets/Scripts/Door/Through_Door.cs
@@ -5,19 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class Through_Door : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-
 	IEnumerator OnTriggerEnter2D(Collider2D other) {
 		// load new scene
 		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 		yield break;
-	}
-
-	// Update is called once per frame
-	void Update () {
-		
 	}
 }
