380f438c003ea2670e905c659532068f1487da98
Justin B <bonnerj@purdue.edu>
2018-03-29 19:57:37 -0400

    Add new puzzle in room 36

---
+++ Labyrinth Demo/Assets/Scripts/BlueCrystalHit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueCrystalHit : MonoBehaviour {
+
+    public GameObject enemyToSpawn;
+
+    void OnTriggerEnter2D(Collider2D coll) {
+        if (coll.tag == "Sword") {
+            GameObject go = Instantiate(enemyToSpawn);
+            go.transform.position = this.transform.position;
+        } 
+    }
+}
