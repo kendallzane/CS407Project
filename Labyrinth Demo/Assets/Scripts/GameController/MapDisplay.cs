973022cd7e3d411a140b3912145c0e0f653a1ce0
Michael Parr <trumpetdude213@yahoo.com>
2018-03-29 16:31:03 -0400

    Midway through map script
    Got rid of anti-aliasing on map images, pulling to see inventory

---
+++ Labyrinth Demo/Assets/Scripts/GameController/MapDisplay.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDisplay : MonoBehaviour {
+
+	//constants
+	const int BASE = 0;
+	const int EARTH = 1;
+	const int FIRE = 2;
+	const int WATER = 3;
+	const int WIND = 4;
+
+	//references
+	private GameController gc;
+
+	//variables
+	public int mapLayer;			//which layer is this map?
+	public GameObject[] rooms;		//array of all the rooms
+	private int toAdd = 0;			//account for the previous layer's maps
+
+	// Use this for initialization
+	void Start () {
+		switch (mapLayer) {
+		case BASE:
+			toAdd = 0;
+			break;
+		case EARTH:
+			toAdd = 6;
+			break;
+		case FIRE:
+			toAdd = 29;
+			break;
+		case WATER:
+			toAdd = 58;
+			break;
+		case WIND:
+			toAdd = 85;
+			break;
+		}
+		foreach (GameObject room in rooms) {
+			room.SetActive (false);
+		}
+		GameObject gcObj = GameObject.FindGameObjectWithTag ("GameController");
+		gc = gcObj.GetComponent<GameController> ();
+	}
+
+	/// <summary>
+	/// Called every time the map is opened, to make sure that only the explored rooms are showing.
+	/// </summary>
+	public void UpdateMap () {
+		for(int i = 0; i < rooms.Length; i++) {
+			if (gc != null && gc.roomsExplored[i + toAdd]) {
+				rooms[i].SetActive (true);
+			}
+		}
+	}
+}
