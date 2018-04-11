9985cf56e678800d18da8b9c469a491c056f8783
Allen Nguyen <nguye225@purdue.edu>
2018-04-11 19:25:08 -0400

    Added some signs to Entrance and Central
    Added signs to provide instructions and background to the game

---
+++ Labyrinth Demo/Assets/Scripts/CentralSign.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CentralSign : MonoBehaviour {
+    public GameObject CentralSignCanvas;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.tag == "Player")
+        {
+            Debug.Log("Hit Sign");
+            CentralSignCanvas.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("Not sensing Main Character");
+            Debug.Log(coll.tag);
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Exited");
+        CentralSignCanvas.SetActive(false);
+    }
+}
---
+++ Labyrinth Demo/Assets/Scripts/EntranceSign.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntranceSign : MonoBehaviour {
+    public GameObject EntranceSignCanvas;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.tag == "Player")
+        {
+            Debug.Log("Hit Sign");
+            EntranceSignCanvas.SetActive(true);
+        } else
+        {
+            Debug.Log("Not sensing Main Character");
+            Debug.Log(coll.tag);
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Exited");
+        EntranceSignCanvas.SetActive(false);
+    }
+}
