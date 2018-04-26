d912b935183946913a6e88e4bbd2b3eeedf27dd8
Aaron Nordhoff <anordhof@purdue.edu>
2018-04-26 00:01:55 -0400

    fire, water, wind, earth bonus rooms first draft
    fire, water, wind, earth bonus rooms 1st draft

---
+++ Labyrinth Demo/Assets/Scripts/FireBonusEnemyCheck.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBonusEnemyCheck : MonoBehaviour {
+
+	GameObject container;
+
+	GameObject cube;
+
+	//private GameObject gc;
+
+	// Use this for initialization
+	void Start () {
+		container = GameObject.Find ("Puzzle 5");
+		cube = container.transform.Find ("cube").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (GameObject.FindWithTag("Enemy") != null)
+        {
+            // Enemies exist
+
+        }
+        else
+        {
+            // No enemies left
+            Destroy(cube);
+        }
+    }
+}
---
+++ Labyrinth Demo/Assets/Scripts/Presure Plate/PPlatePuzzle4.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PPlatePuzzle4 : MonoBehaviour {
+
+	GameObject container;
+
+	PPlate pPlate1;
+	PPlate pPlate2;
+	PPlate pPlate3;
+
+
+	GameObject cube;
+
+	//private GameObject gc;
+
+	// Use this for initialization
+	void Start () {
+		container = GameObject.Find ("Puzzle 4");
+
+		PPlate[] temp = GetComponentsInChildren<PPlate> ();
+		pPlate1 = temp [0];
+		pPlate2 = temp [1];
+		pPlate3 = temp [2];
+
+
+		//cube = GameObject.Find ("cube");
+
+		cube = container.transform.Find ("cube").gameObject;
+		/* need GameController
+		gc = GameObject.Find ("GameController");
+
+		if (gc.GetComponent<GameController> ().roomPuzzle [25]) {
+			cube.SetActive (false);
+		}
+		*/
+	}
+
+	// Update is called once per frame
+	void Update () {
+        /*
+		if (pPlate2.getIsOn()) {
+			if (pPlate1.getIsOn () && !(pPlate3.getIsOn ())) {
+				pPlate1.off ();
+				pPlate2.off ();
+			}
+			if (pPlate3.getIsOn () && pPlate1.getIsOn ()) {
+				Destroy (cube);
+				//gc.GetComponent<GameController> ().roomPuzzle [25] = true;
+			}
+		}
+		if (pPlate1.getIsOn() && !(pPlate2.getIsOn())) {
+			pPlate1.off ();
+		}
+		if (pPlate3.getIsOn() && !(pPlate2.getIsOn())) {
+			pPlate3.off ();
+		}
+        */
+        if (pPlate1.getIsOn () && pPlate2.getIsOn () && pPlate3.getIsOn ())
+        {
+            Destroy(cube);
+        }
+	}
+}
+
+
+
